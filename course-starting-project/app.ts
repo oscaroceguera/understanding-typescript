ac44d9534ed37ac5c23b6c081b9165e251bc7ffb
oscar <oscaroceguera@gmail.com>
2020-05-20 02:10:21 -0600

    Cobine

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,35 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "Maximilian",
-//   age: 30,
-//   hobbies: ["sports", "cooking"],
-//   role: [2, "author"]
-// };
-
-enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' }
-
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["sports", "cooking"],
-  role: Role.ADMIN
-};
-
-// person.role = [10, 'admin']
-
-let favoriteActivities: string[]
-favoriteActivities = ['sports']
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase())
+function combine(input1: number | string, input2: number | string) {
+  let result
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-if (person.role === Role.AUTHOR) {
-  console.log('is admin')
-}
+const combinedAges = combine(30, 26)
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna')
+console.log("combinedNames", combinedNames)

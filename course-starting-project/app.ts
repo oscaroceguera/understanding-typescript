64f615cce6fe9f358750263e0921d8d7e7474a85
oscar <oscaroceguera@gmail.com>
2020-05-20 01:57:49 -0600

    enum

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,16 +1,25 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"]
+// };
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' }
+
+const person = {
   name: "Maximilian",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"]
+  role: Role.ADMIN
 };
 
-person.role = [10, 'admin']
+// person.role = [10, 'admin']
 
 let favoriteActivities: string[]
 favoriteActivities = ['sports']
@@ -18,5 +27,9 @@ favoriteActivities = ['sports']
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby)
+  console.log(hobby.toUpperCase())
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is admin')
 }

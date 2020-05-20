4327808e64df5e63756886c0f81ef1900ac6b7df
oscar <oscaroceguera@gmail.com>
2020-05-20 02:48:08 -0600

    Function types and callbacks

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,33 +1,23 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: 'as-number' | 'as-text'
-) {
-  let result
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
-  
+function add(n1: number, n2: number) {
+  return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number')
-console.log('combinedAges', combinedAges);
+function printResult(num: number): void {
+  console.log('Result ' + num)
+}
 
-const combinedStringAges = combine('30', '26', 'as-number')
-console.log('combinedStringAges', combinedStringAges);
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result)
+}
 
-const combinedNames = combine('Max', 'Anna', 'as-text')
-console.log("combinedNames", combinedNames)
+printResult(add(5, 12))
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add
+
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+})

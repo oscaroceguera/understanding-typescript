978e2e0aa1ced84d6add6e25c68cacf5c56cd2bb
oscar <oscaroceguera@gmail.com>
2020-05-19 23:33:04 -0600

    algo

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,22 @@
-console.log('Your code goes here...');
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["sports", "cooking"],
+  role: [2, "author"]
+};
+
+person.role = [10, 'admin']
+
+let favoriteActivities: string[]
+favoriteActivities = ['sports']
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby)
+}

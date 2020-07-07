589f988c3b9f062fc7c12953f6af9e40a6c84942
oscar <oscaroceguera@gmail.com>
2020-07-07 00:11:05 -0600

    Using namespaces AMD

---
+++ drag-drop/src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+    // autobind decorator
+  export function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const bounFn = originalMethod.bind(this);
+        return bounFn;
+      }
+    }
+    return adjDescriptor;
+  }
+}

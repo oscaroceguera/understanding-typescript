8512344a929297ca02340dd378caa3a5910aa690
oscaroceguera@gmail.com <oscaroceguera@gmail.com>
2020-07-13 02:20:44 -0600

    init React

---
+++ 2-complete-ts-guide-2020/rrts/src/components/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Todo, fetchTodos} from '../actions'
+import {StoreState} from '../reducers'
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  onBtnClick = (): void => {
+    this.props.fetchTodos()
+  }
+  renderList(): JSX.Element[]{
+    return this.props.todos.map((todo: Todo) => {
+    return <div key={todo.id}>{todo.title}</div>
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onBtnClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return {
+    // todos: state.todos
+    todos
+  }
+}
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);

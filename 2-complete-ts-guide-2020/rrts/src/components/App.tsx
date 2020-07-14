216b5ea3f77e43117ae4945d1c1bfa9992cd0a78
oscar <oscaroceguera@gmail.com>
2020-07-14 01:13:14 -0600

    TS + React + Redux

--- 2-complete-ts-guide-2020/rrts/src/components/App.tsx
+++ 2-complete-ts-guide-2020/rrts/src/components/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Todo, fetchTodos} from '../actions'
+import {Todo, fetchTodos, deleteTodo} from '../actions'
 import {StoreState} from '../reducers'
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false }
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length){
+      this.setState({fetching: false})
+    }
+  }
+
   onBtnClick = (): void => {
     this.props.fetchTodos()
+    this.setState({fetching: true})
   }
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id)
+  }
+
   renderList(): JSX.Element[]{
     return this.props.todos.map((todo: Todo) => {
-    return <div key={todo.id}>{todo.title}</div>
+      return (
+        <div
+          style={{border: '1px solid red', padding: '1em'}}
+          onClick={() => this.onTodoClick(todo.id)}
+          key={todo.id}
+        >
+          {todo.title}
+        </div>
+      )
     })
   }
+
   render() {
     return (
       <div>
         <button onClick={this.onBtnClick}>Fetch</button>
+        {this.state.fetching ? 'LOADIING' : null}
         {this.renderList()}
       </div>
     );
@@ -34,4 +66,7 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   }
 }
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos, deleteTodo }
+)(_App);
--- 2-complete-ts-guide-2020/rrts/src/reducers/todos.ts
+++ 2-complete-ts-guide-2020/rrts/src/reducers/todos.ts
@@ -1,13 +1,12 @@
-import {Todo, FetchTodosAction} from '../actions'
-import {ActionsTypes} from '../actions/types'
+import { Todo, Action, ActionsTypes} from '../actions'
 
-export const todosReducer = (
-  state: Todo[] = [],
-  action: FetchTodosAction
-) => {
+
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch(action.type) {
     case ActionsTypes.fetchTodos:
       return action.payload
+    case ActionsTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload)
     default:
       return state;
   }

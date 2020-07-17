edaf33ccaf8950d0f47cd60b0bbf2d6cc63832fe
oscar <oscaroceguera@gmail.com>
2020-07-17 00:42:34 -0600

    React hook todo list

--- 4-todo-list/src/index.tsx
+++ 4-todo-list/src/index.tsx
@@ -1,17 +1,77 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import TodoForm from './components/todo-form';
+import TodoList from './components/todo-list';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { TodoInterface } from './interfaces';
+
+import './styles/styles.css';
+
+const TodoListApp = () => {
+  const [todos, setTodos] = React.useState<TodoInterface[]>([]);
+
+  function handleTodoCreate(todo: TodoInterface) {
+    const newTodoState: TodoInterface[] = [...todos];
+
+    newTodoState.push(todo);
+
+    setTodos(newTodoState);
+  }
+
+  function handleTodoUpdate(
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: string
+  ) {
+    const newtodoState: TodoInterface[] = [...todos];
+
+    newtodoState.find((todo: TodoInterface) => todo.id === id)!.text =
+      event.target.value;
+
+    setTodos(newtodoState);
+  }
+
+  function handleTodoRemove(id: string) {
+    const newTodosState: TodoInterface[] = todos.filter(
+      (todo: TodoInterface) => todo.id !== id
+    );
+
+    setTodos(newTodosState);
+  }
+
+  function handleTodoComplete(id: string) {
+    const newTodosState: TodoInterface[] = [...todos];
+
+    newTodosState.find(
+      (todo: TodoInterface) => todo.id === id
+    )!.isCompleted = !newTodosState.find(
+      (todo: TodoInterface) => todo.id === id
+    )!.isCompleted;
+
+    setTodos(newTodosState);
+  }
+
+  function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.value.length === 0) {
+      event.target.classList.add('todo-input-error');
+    } else {
+      event.target.classList.remove('todo-input-error');
+    }
+  }
+  return (
+    <div className='todo-list-app'>
+      <TodoForm todos={todos} handleTodoCreate={handleTodoCreate} />
+
+      <TodoList
+        todos={todos}
+        handleTodoUpdate={handleTodoUpdate}
+        handleTodoRemove={handleTodoRemove}
+        handleTodoComplete={handleTodoComplete}
+        handleTodoBlur={handleTodoBlur}
+      />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+render(<TodoListApp />, rootElement);
